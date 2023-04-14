0f7b8b53f69b712e3be0fc5bac494ec2af1aa0fd
cpsoucy <96217410+cpsoucy@users.noreply.github.com>
2023-04-14 19:32:26 -0400

    attempting to get API fetching to work (WIP)

--- src/routes/posts/+page.server.js
+++ src/routes/posts/+page.server.js
@@ -1,10 +1,34 @@
 import { posts } from './data.js';
 
-export function load() {
+export async function load({fetch}) {
+  const rand = Math.floor(Math.random() * 10000) + 1;
+
+  const url = 'https://random-d.uk/api/v2' + rand + '&limit=12'; //what does &limit do here?
+  const request = await fetch(url);
+  const results = await request.json();
+  const ducks = results.data;
+
   return {
+    ducks,
     summaries: posts.map((post) => ({
-      slug: post.slug,
-      title: post.title
+    slug: post.slug,
+    title: post.title
     }))
   };
 }
+
+// export async function load({ fetch }) {  
+//   const page = Math.floor(Math.random() * 10000) + 1;
+
+//   const url = 'https://api.artic.edu/api/v1/artworks?fields=image_id,title,artist_title,date_display,medium_display&page=' + page + '&limit=12';
+
+//   const artReq = await fetch(url);
+  
+//   const artRes = await artReq.json();
+
+//   const artworks = artRes.data;
+
+//   return {
+//     artworks
+//   }
+// }
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -2,6 +2,16 @@
     export let data;
   </script>
 
+  <p>this is posts</p>
+
+  <article>
+    <!-- {#each duck as {}}} -->
+    <figure>
+      <img src="https://random-d.uk/api/v2" alt="">
+      <!-- {"url": "https://random-d.uk/api/images/51.jpg", "message": "Powered by random-d.uk"} -->
+    </figure>
+  </article>
+
   <aside>
     <nav class="sidebar">
         {#each data.summaries as {slug, title}}
